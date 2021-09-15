1320918ae48fd5a4cabe91fa688df567a4d0877a
irfanwashere <irfan.a@allentics.com>
2021-09-15 14:21:21 +0530

    modify mongoose connection

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json());
 mongoose
   .connect(
-    'mongodb+srv://iirfanansarii:NRv1dtKbMHQYg77g@cluster0.9l86r.mongodb.net/dev?authSource=admin&replicaSet=atlas-edmn0k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.9l86r.mongodb.net/${process.env.MONGO_DB_DATABASE}?authSource=admin&replicaSet=atlas-edmn0k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    },
+    }
   )
   .then(() => {
     console.log('Database Connected');
